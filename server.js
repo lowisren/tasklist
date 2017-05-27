e4f8372da65375aa96cfcb94be379b101f4e6fec
lowisren <lowisren@gmail.com>
2017-05-27 13:50:31 -0400

    updates to middleware and server

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.use('/users', users);
 
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 4000));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+ app.get('port'));
